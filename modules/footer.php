e43746888df170650107278dafa6bbdcd9471032
Chris <chrisnewcarson@gmail.com>
2019-02-22 14:33:20 +0300

    added the footer section

--- modules/footer.php
+++ modules/footer.php
@@ -1,1 +1,79 @@
-Footer
+<footer class="sect site-footer">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<h4 class="section-title">For Businesses</h4>
+
+
+				<ul class="footer-links">
+					<li><a href="">Sokoswift</a></li>
+					<li><a href="">Messanger</a></li>
+					<li><a href="">SQLPro 4.0</a></li>
+					<li><a href="">Web Design</a></li>
+					<li><a href="">Ecommerce</a></li>
+				</ul>
+
+
+
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				
+
+
+				<h4 class="section-title">For Writers</h4>
+
+				
+				<ul class="footer-links">
+					<li><a href="">Join Training</a></li>
+					<li><a href="">Find Work</a></li>
+					<li><a href="">E-Books</a></li>
+					<li><a href="">Blog</a></li>
+					<!-- <li><a href="">Ecommerce</a></li> -->
+				</ul>
+
+
+
+
+			</div>
+
+
+
+
+
+
+
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				
+				<h4 class="section-title">For Everyone</h4>
+
+				
+				<ul class="footer-links">
+					<li><a href="">+254 716 634 902</a></li>
+					<li><a href="">hello@clearphrases.com</a></li>
+					<li><a href="">Facebook</a></li>
+					<li><a href="">Twitter</a></li>
+					<li><a href="">Instagram</a></li>
+				</ul>
+
+
+
+			</div>
+
+
+
+
+			<div class="col-md-12 copyright-section">
+
+				<p><small>Copyright &copy; 2019. Clearphrases Limited. All Rights Reserved.</small></p>
+				
+			</div>
+
+
+		</div>
+
+
+
+	</div>
+</footer>
